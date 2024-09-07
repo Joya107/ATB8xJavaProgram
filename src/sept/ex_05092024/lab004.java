73e60e84affd7728b3d8548db4abd92ceb350ece
Joya107 <shaikhjoya212@gmail.com>
2024-09-07 15:36:19 +0530

    Add the files

---
+++ src/sept/ex_05092024/lab004.java
@@ -0,0 +1,9 @@
+package sept.ex_05092024;
+
+public class lab004 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+        System.out.println(2-2);
+    }
+}
