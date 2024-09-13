b2e006b4e0b1892d89d85694473da683ddb92d2f
Joya107 <shaikhjoya212@gmail.com>
2024-09-13 18:58:18 +0530

    Add the files

---
+++ src/sept/ex_13092024/lab002.java
@@ -0,0 +1,14 @@
+package sept.ex_13092024;
+
+public class lab002 {
+    public static void main(String[] args) {
+        double a = 34;
+        double b = 10;
+
+        System.out.printf("a + b = %f%n", a + b);    // Addition
+        System.out.printf("a - b = %f%n", a - b);    // Subtraction
+        System.out.printf("a * b = %f%n", a * b);    // Multiplication
+        System.out.printf("a / b = %f%n", a / b);    // Division
+        System.out.printf("a %% b = %f%n", a % b);   // Modulus (remainder)
+    }
+}
